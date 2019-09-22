71b98a0837558f935f09fe2fcaf979c249e245d2
Celia Lafond <celia.lafond69@gmail.com>
2019-09-23 00:13:38 +0200

    create subvention page

---
+++ src/pages/Subvention.page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../modules/Layout/Layout';
+import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
+import CommentBlock from '../modules/CommentBlock/CommentBlock';
+
+
+const Subvention = () => (
+  <Layout>
+    <Breadcrumb></Breadcrumb>
+    <CommentBlock></CommentBlock>
+  </Layout>
+)
+
+export default Subvention;
